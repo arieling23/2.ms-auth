fbb7d95b5d0d04760c083df9d90536f321c2a877
ArielInguillay <arieling123@gmail.com>
2025-07-16 18:55:08 -0500

    feat(auth): initialize server with MongoDB, CORS, and event consumer

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,14 +2,19 @@ const express = require('express');
 const {
   loginUser,
   changePassword,
-  updatePasswordWithEmail
+  updatePasswordWithEmail, 
 } = require('../controllers/authController');
 const verifyJWT = require('../middlewares/verifyJWT');
 
 const router = express.Router();
 
+
 router.post('/login', loginUser);
+
+
 router.put('/change-password', verifyJWT, changePassword);
+
+
 router.put('/update-password', updatePasswordWithEmail);
 
 module.exports = router;
