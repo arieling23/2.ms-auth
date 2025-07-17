9c089080a52a42ad9ccb9a547581351d50fe3630
ArielInguillay <arieling123@gmail.com>
2025-07-16 19:34:26 -0500

    feat(app): add global middlewares for CORS and JSON parsing

--- src/app.js
+++ src/app.js
@@ -17,8 +17,9 @@ const corsOptions = {
 };
 
 
+// Middlewares
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); 
+app.options(/^\/api\/.*$/, cors(corsOptions));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
